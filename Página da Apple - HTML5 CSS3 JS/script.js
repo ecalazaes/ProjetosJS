ea4679fef40c0a37de34a6908e017942fd75cf4c
Erick Calazães <ecalazaes@gmail.com>
2024-02-01 01:06:23 -0300

    Update script.js

--- Página da Apple - HTML5 CSS3 JS/script.js
+++ Página da Apple - HTML5 CSS3 JS/script.js
@@ -42,26 +42,17 @@ function trocarImagem() {
     '[name="opcao-imagem"]:checked'
   ).id;
   imagemSelecionada = idOpcaoSelecionada.charAt(0);
-  imagemVisualizacao.src =
-    "./imagens/opcoes-cores/" +
-    opcoesCores[corSelecionada].pasta +
-    "/imagem-" +
-    imagemSelecionada +
-    ".jpeg";
+  imagemVisualizacao.src = "./imagens/opcoes-cores/" + opcoesCores[corSelecionada].pasta + "/imagem-" + imagemSelecionada + ".jpeg";
 }
 
 function trocarTamanho() {
   // Atualizar variável de controle do tamanho da caixa
-  const idOpcaoSelecionada = document.querySelector(
-    '[name="opcao-tamanho"]:checked'
-  ).id;
+  const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id;
   tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
+ 
   // Mudar o título do produto
-  tituloProduto.innerText =
-    "Pulseira loop esportiva " +
-    opcoesCores[corSelecionada].nome +
-    " para caixa de " +
-    opcoesTamanho[tamanhoSelecionado];
+  tituloProduto.innerText = "Pulseira loop esportiva " + opcoesCores[corSelecionada].nome + " para caixa de " + opcoesTamanho[tamanhoSelecionado];
+ 
   // Mudar o tamanho da imagem de acordo com a opção
   if (opcoesTamanho[tamanhoSelecionado] === "41 mm") {
     imagemVisualizacao.classList.add("caixa-pequena");
@@ -72,36 +63,20 @@ function trocarTamanho() {
 
 function trocarCor() {
   // Atualizar cor selecionada
-  const idOpcaoSelecionada = document.querySelector(
-    '[name="opcao-cor"]:checked'
-  ).id;
+  const idOpcaoSelecionada = document.querySelector('[name="opcao-cor"]:checked').id;
   corSelecionada = idOpcaoSelecionada.charAt(0);
+ 
   // Trocar título da página
-  tituloProduto.innerText =
-    "Pulseira loop esportiva " +
-    opcoesCores[corSelecionada].nome +
-    " para caixa de " +
-    opcoesTamanho[tamanhoSelecionado];
+  tituloProduto.innerText ="Pulseira loop esportiva " + opcoesCores[corSelecionada].nome + " para caixa de " + opcoesTamanho[tamanhoSelecionado];
+ 
   // Trocar nome da cor
   nomeCor.innerText = "Cor - " + opcoesCores[corSelecionada].nome;
+  
   // Trocar imagens das miniaturas exibidas
-  miniaturaImagem0.src =
-    "./imagens/opcoes-cores/" +
-    opcoesCores[corSelecionada].pasta +
-    "/imagem-0.jpeg";
-  miniaturaImagem1.src =
-    "./imagens/opcoes-cores/" +
-    opcoesCores[corSelecionada].pasta +
-    "/imagem-1.jpeg";
-  miniaturaImagem2.src =
-    "./imagens/opcoes-cores/" +
-    opcoesCores[corSelecionada].pasta +
-    "/imagem-2.jpeg";
+  miniaturaImagem0.src = "./imagens/opcoes-cores/" + opcoesCores[corSelecionada].pasta + "/imagem-0.jpeg";
+  miniaturaImagem1.src = "./imagens/opcoes-cores/" + opcoesCores[corSelecionada].pasta + "/imagem-1.jpeg";
+  miniaturaImagem2.src = "./imagens/opcoes-cores/" + opcoesCores[corSelecionada].pasta + "/imagem-2.jpeg";
+ 
   // Trocar imagem de visualizacao
-  imagemVisualizacao.src =
-    "./imagens/opcoes-cores/" +
-    opcoesCores[corSelecionada].pasta +
-    "/imagem-" +
-    imagemSelecionada +
-    ".jpeg";
+  imagemVisualizacao.src = "./imagens/opcoes-cores/" + opcoesCores[corSelecionada].pasta + "/imagem-" + imagemSelecionada + ".jpeg";
 }
